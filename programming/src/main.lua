0903dd1df6496853d1f5f27d27d2805a27d68fe5
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2016-10-31 00:26:53 +0100

    Added a draw queue and an update queue to main.lua

--- programming/src/main.lua
+++ programming/src/main.lua
@@ -1,5 +1,9 @@
+local drawqueue, stepqueue
+
 function love.load()
 	print "Hello World :)"
+	drawqueue = require"eventqueue".new("draw")
+	stepqueue = require"eventqueue".new("update")
 end
 
 function love.quit()
@@ -7,11 +11,11 @@ function love.quit()
 end
 
 function love.draw()
-	
+	drawqueue:runall()
 end
 
 function love.update(dt)
-	
+	stepqueue:runall(dt)
 end
 
 function love.keypressed(key, scancode, isrepeat)
